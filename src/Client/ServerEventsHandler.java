4a1b7025183c10f638b6ba00e0a80233547b625f
mrx7355608 <mrx7355608@gmail.com>
2024-07-09 03:31:57 +0500

    game starts after both players have connected to server

--- src/Client/ServerEventsHandler.java
+++ src/Client/ServerEventsHandler.java
@@ -1,5 +1,8 @@
 package Client;
 
+import Game.Board;
+import Game.Checkers;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,9 +11,11 @@ import java.net.Socket;
 public class ServerEventsHandler implements Runnable {
 
     private final Socket socket;
+    private final Checkers game;
 
-    public ServerEventsHandler(Socket s) {
+    public ServerEventsHandler(Socket s, Checkers checkers) {
         this.socket = s;
+        this.game = checkers;
     }
 
     @Override
@@ -18,7 +23,18 @@ public class ServerEventsHandler implements Runnable {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
             while (true) {
-                System.out.println(in.readLine());
+                String input = in.readLine();
+                String event = input.split(":")[0];
+                String message = input.split(":")[1];
+
+                switch (event) {
+                    case "START_GAME" -> {
+                        this.game.remove(this.game.initialLabel);
+                        this.game.add(new Board());
+                        this.game.repaint();
+                    }
+                }
+
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
--- src/Game/Checkers.java
+++ src/Game/Checkers.java
@@ -1,11 +1,14 @@
 package Game;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.net.Socket;
 import Client.*;
 
 public class Checkers extends JFrame {
+
+    public JLabel initialLabel;
 
     public Checkers() {
         super.setSize(495, 519);
@@ -13,8 +16,10 @@ public class Checkers extends JFrame {
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         super.setResizable(false);
         super.setLocationRelativeTo(null);
-        Board board = new Board();
-        super.add(board);
+
+        initialLabel = new JLabel("Waiting for another player to join...", JLabel.CENTER);
+        initialLabel.setFont(new Font("Fira Code", Font.BOLD, 20));
+        super.add(initialLabel);
     }
 
     public static void main(String[] args) {
@@ -27,12 +32,13 @@ public class Checkers extends JFrame {
             Client client = Client.getInstance();
             Socket socket = client.connect(ip);
 
-            // Spawn a new thread to receive server messages in parallel with game gui
-            Thread thread = new Thread(new ServerEventsHandler(socket));
-            thread.start();
-
             // Start game gui on main thread
-            new Checkers().setVisible(true);
+            Checkers checkers = new Checkers();
+            checkers.setVisible(true);
+
+            // Spawn a new thread to receive server messages in parallel with game gui
+            Thread thread = new Thread(new ServerEventsHandler(socket, checkers));
+            thread.start();
 
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Unable to connect to server", "Connection Failed", JOptionPane.ERROR_MESSAGE);
